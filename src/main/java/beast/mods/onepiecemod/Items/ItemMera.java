713f8bc01f013ecaebcc6663c613fe1349c9ad75
BeastInAMask <BeastInAMask@gmail.com>
2018-05-21 19:20:07 -0700

    updated the classes and tried to add a gui

---
+++ src/main/java/beast/mods/onepiecemod/Items/ItemMera.java
@@ -0,0 +1,21 @@
+package src.main.java.beast.mods.onepiecemod.Items;
+
+
+import net.minecraft.item.ItemFood;
+
+
+
+public class ItemMera extends ItemFood {
+
+    public ItemMera() {
+        super(20, 1.0F, false);
+    }
+
+
+
+
+    }
+
+
+
+
